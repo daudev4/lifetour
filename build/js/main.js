f34fae25df22a8a362889e6ff8bb1a9e0e0fb58c
daudev4 <daud_08@mail.ru>
2020-02-17 03:13:48 +0300

    Исправляет баги в IE11, добавляет первоначальную реализацию слайдера

--- build/js/main.js
+++ build/js/main.js
@@ -2,26 +2,32 @@
 
 (function () {
 
-  var Slider = function (selector) {
-    this.mainElement = document.querySelector(selector);
-    this.body = this.mainElement.querySelector('.slider__body');
+  var Slider = function (mainElement) {
+    var self = this;
+    this.body = mainElement.querySelector('.slider__body');
     this.list = this.body.querySelector('.slider__list');
-    this.listWidth = parseFloat(getComputedStyle(this.list).width);
-    this.listDefaultPosition = 0;
+    this.listWidth = parseFloat(this.list.scrollWidth);
     this.items = this.list.querySelectorAll('.slider__item');
-    this.itemWidth = parseFloat(getComputedStyle(this.items[0]).width);
-    this.controls = this.mainElement.querySelector('.slider__controls');
+    this.itemWidth = parseFloat(this.items[1].offsetWidth);
+    this.controls = mainElement.querySelector('.slider__controls');
+    this.controls.classList.remove('.slider__controls_hidden');
     this.controlLeft = this.controls.querySelector('.slider__control_left');
     this.controlRight = this.controls.querySelector('.slider__control_right');
-    this.step = this.itemWidth / this.listWidth * 100;
+    this.controlRight.onclick = function () {
+      self.slideScroll(self.list, self.controlRight, self.itemWidth);
+    };
+    this.controlLeft.onclick = function () {
+      self.slideScroll(self.list, self.controlLeft, self.itemWidth);
+    };
   };
 
-  Slider.prototype.setDefault = function () {
-    this.body.classList.add('.slider__body_no-scroll');
-    this.controls.classList.remove('.slider__controls_hidden');
-    this.controlLeft.disabled = true;
-    this.controlRight.disabled = false;
-    this.list.style.transform = 'translate(' + this.listDefaultPosition + ')';
+  Slider.prototype.slideScroll = function (element, control, step) {
+    element.scrollLeft += control === this.controlLeft ? -step : +step;
   };
 
+  var sliderElements = document.querySelectorAll('.slider');
+  var sliders = [];
+  Array.prototype.forEach.call(sliderElements, function (element) {
+    sliders.push(new Slider(element));
+  });
 })();
